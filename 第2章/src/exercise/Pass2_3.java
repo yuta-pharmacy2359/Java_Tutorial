4c8dead8f6fdc7182a1771012bf244c4d0718ac0
Yuta <yuta.shogai@gmail.com>
2021-04-27 00:57:31 +0900

    Java入門 第2章 終了

---
+++ 第2章/src/exercise/Pass2_3.java
@@ -0,0 +1,12 @@
+package exercise;
+
+public class Pass2_3 {
+	public static void main(String[] args) {
+		System.out.print(1000);
+		System.out.println("田中弘");
+		System.out.print(1010);
+		System.out.println("谷川太郎");
+		System.out.print(1020);
+		System.out.println("鈴木誠");
+	}
+}
