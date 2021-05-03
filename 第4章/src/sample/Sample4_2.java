5b286e478a983e6e34b6dc62112985dcbda07109
Yuta <yuta.shogai@gmail.com>
2021-05-03 23:49:22 +0900

    Java入門 第4-2章終了

---
+++ 第4章/src/sample/Sample4_2.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Sample4_2 {
+	public static void main(String[] args) {
+		int ken = 10;
+		int tanka, total;
+
+		tanka = 50;
+		total = ken * tanka;
+		System.out.println(total);
+	}
+}
