31d465b375ab69b16de52dfa342ddfc10955fd2b
Yuta <yuta.shogai@gmail.com>
2021-05-04 20:57:29 +0900

    Java入門 第4章終了

---
+++ 第4章/src/exercise/Pass4_4.java
@@ -0,0 +1,10 @@
+package exercise;
+
+public class Pass4_4 {
+	public static void main(String[] args) {
+		double area, radius = 12.5;
+		area = radius * radius * 3.14;
+		System.out.print("面積=");
+		System.out.println(area);
+	}
+}
