0e5438a80ea0cb96d3f9c6bcae97c2be474e1161
Yuta <yuta.shogai@gmail.com>
2021-05-08 00:45:32 +0900

    Java入門 練習6-2終了

---
+++ 第6章/src/exercise/Ex6_2_1.java
@@ -0,0 +1,13 @@
+package exercise;
+
+public class Ex6_2_1 {
+	public static void main(String[] args) {
+		double a = 30.51, b = 2.68;
+		System.out.println(Math.round(a / b));
+		System.out.println(Math.pow(a, b));
+		System.out.println(Math.min(a, b));
+		System.out.println(Math.sqrt(a) + Math.sqrt(b));
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+	}
+}
---
+++ 第6章/src/sample/Sample6_2.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Sample6_2 {
+	public static void main(String[] args) {
+		double x = Math.pow(2, 8);
+		System.out.println("2の8乗=" + x);
+		double a = Math.random();
+		System.out.println("乱数=" + a);
+	}
+}
