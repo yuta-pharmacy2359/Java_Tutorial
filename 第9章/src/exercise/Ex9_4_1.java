95f34240eb8ae8e7defd7e175019367f52fa1279
Yuta <yuta.shogai@gmail.com>
2021-05-18 23:43:34 +0900

    Java入門 練習9-4終了

---
+++ 第9章/src/exercise/Ex9_4_1.java
@@ -0,0 +1,10 @@
+package exercise;
+
+public class Ex9_4_1 {
+	public static void main(String[] args) {
+		String str = "こんにちは";
+		"word".equals(str);
+		str.equals("Hello");
+		str.equals("こんにちは");
+	}
+}
---
+++ 第9章/src/sample/Sample9_3B.java
@@ -0,0 +1,10 @@
+package sample;
+
+import lib.Input;
+
+public class Sample9_3B {
+	public static void main(String[] args) {
+		String str = Input.getString();
+		System.out.println(str == "apple");
+	}
+}
