378b33ef5b7008b541b1177eca7da66d07e8c412
Yuta <yuta.shogai@gmail.com>
2021-08-20 23:56:45 +0900

    Java入門 Sample11-1終了

---
+++ 第11章/src/sample/Sample11_1.java
@@ -0,0 +1,13 @@
+package sample;
+import lib.Input;
+
+public class Sample11_1 {
+	public static void main(String[] args) {
+		int n = Input.getInt();
+		if(n == 1) {
+			System.out.println("賛成");
+		} else {
+			System.out.println("反対");
+		}
+	}
+}
