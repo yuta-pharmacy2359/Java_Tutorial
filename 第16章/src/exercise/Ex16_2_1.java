0b7dab8d83e63bf631009300afbbecf0f85d4030
Yuta <yuta.shogai@gmail.com>
2021-09-13 01:01:33 +0900

    Java入門 第16-2章終了

---
+++ 第16章/src/exercise/Ex16_2_1.java
@@ -0,0 +1,9 @@
+package exercise;
+
+public class Ex16_2_1 {
+	public static void main(String[] args) {
+		Order o1 = new Order("ICBK61", "2020-07-11", 2100, 5, true);
+		Order o2 = new Order("ICBK62", "2020-09-02", 1050, 10, false);
+		Order o3 = new Order("ICBK63", "2020-07-15", 1050, 12, true);
+	}
+}
---
+++ 第16章/src/sample/Sample16_2.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Sample16_2 {
+	public static void main(String[] args) {
+		Sales s1 = new Sales("SY-200", "冷蔵庫", 50000, 20, true);
+		Sales s2 = new Sales("TB-100", "洗濯機", 30000, 15, true);
+		Sales s3 = new Sales("AX-551", "テレビ", 10000, 32, false);
+	}
+}
